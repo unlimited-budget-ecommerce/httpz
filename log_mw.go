8ab6e1c44b1743bb5de494d9c3c572992f0f00f5
sat <77387934+sixsat@users.noreply.github.com>
2025-05-05 22:43:51 +0700

    update log mw and add TODO

--- log_mw.go
+++ log_mw.go
@@ -1,6 +1,7 @@
 package httpz
 
 import (
+	"encoding/json"
 	"log/slog"
 
 	"github.com/go-resty/resty/v2"
@@ -13,11 +14,20 @@ func logRequest(cfg *config) resty.RequestMiddleware {
 			return nil
 		}
 
-		cfg.logger.Info("[HTTPZ][OUTGOING REQUEST]",
+		logger := cfg.logger.With(
 			slog.String(string(semconv.URLFullKey), req.URL),
 			slog.String(string(semconv.HTTPRequestMethodKey), req.Method),
-			slog.Any("http.request.header", req.Header),
-			slog.Any("http.request.body", req.Body),
+			slog.Any("http.request.header", req.Header), // TODO: wraps with mask map
+		)
+
+		body, err := json.Marshal(req.Body)
+		if err != nil {
+			logger.Error("[HTTPZ][OUTGOING REQUEST] error marshalling request body: " + err.Error())
+			return err
+		}
+
+		logger.Info("[HTTPZ][OUTGOING REQUEST] success",
+			slog.Any("http.request.body", body), // TODO: wraps with mask []byte
 		)
 
 		return nil
@@ -35,8 +45,8 @@ func logResponse(cfg *config) resty.ResponseMiddleware {
 			slog.String(string(semconv.HTTPRequestMethodKey), res.Request.Method),
 			slog.Int64(semconv.HTTPClientRequestDurationName, res.Time().Milliseconds()),
 			slog.Int(string(semconv.HTTPResponseStatusCodeKey), res.StatusCode()),
-			slog.Any("http.response.header", res.Header),
-			slog.Any("http.response.body", res.Body()),
+			slog.Any("http.response.header", res.Header()), // TODO: wraps with mask map
+			slog.Any("http.response.body", res.Body()),     // TODO: wraps with mask []byte
 		)
 
 		if res.IsError() {
