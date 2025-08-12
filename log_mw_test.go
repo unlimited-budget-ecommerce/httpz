d49956cbf6f8973288559523d9ba03003106b2c5
sat <77387934+sixsat@users.noreply.github.com>
2025-08-12 13:32:45 +0700

    update test

--- log_mw_test.go
+++ log_mw_test.go
@@ -3,12 +3,15 @@ package httpz
 import (
 	"bytes"
 	"context"
+	"io"
 	"log/slog"
 	"net/http"
+	"sync"
 	"testing"
 
 	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLogMiddleware(t *testing.T) {
@@ -61,6 +64,7 @@ func TestLogMiddleware(t *testing.T) {
 
 		res, err := clientWithLog.NewRequest(context.Background()).
 			SetHeader("X-Test-Req", "req-header-val").
+			SetQueryParam("test-query", "query-val").
 			SetBody(wantReqBody).
 			SetResult(result).
 			Post(clientWithLog.GetPath("testLog"))
@@ -75,7 +79,7 @@ func TestLogMiddleware(t *testing.T) {
 
 		// request log
 		assert.Contains(t, logs, "[HTTPZ][OUTGOING REQUEST]")
-		assert.Contains(t, logs, `"url.full":"`+server.URL+`/test/log"`)
+		assert.Contains(t, logs, `"url.full":"`+server.URL+`/test/log?test-query=query-val"`)
 		assert.Contains(t, logs, `"http.request.method":"POST"`)
 		assert.Contains(t, logs, `"X-Test-Req":["req-header-val"]`)
 		assert.Contains(t, logs, `"http.request.body":{"input":"ping"}`)
@@ -108,4 +112,73 @@ func TestLogMiddleware(t *testing.T) {
 	})
 
 	// TODO: Add test cases for logging error request, response
+}
+
+func TestConcurrentLogMiddleware(t *testing.T) {
+	type testLogReq struct {
+		Input1 string `json:"input1"`
+		Input2 string `json:"input2"`
+		Input3 string `json:"input3"`
+	}
+	type testLogRes struct {
+		Output1 string `json:"output1"`
+		Output2 string `json:"output2"`
+		Output3 string `json:"output3"`
+	}
+	wantReqBody := testLogReq{Input1: "foo", Input2: "bar", Input3: "baz"}
+	wantResBody := testLogRes{Output2: "out1", Output1: "out2", Output3: "out3"}
+	server := startTestServer(t, testHandler{
+		method: http.MethodPost,
+		path:   "/test/log/concurrent",
+		handlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			var reqBody testLogReq
+
+			err := json.NewDecoder(r.Body).Decode(&reqBody)
+			if err != nil {
+				http.Error(w, "error decoding request body", http.StatusBadRequest)
+				return
+			}
+			assert.ObjectsAreEqual(wantReqBody, reqBody)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			err = json.NewEncoder(w).Encode(wantResBody)
+			if err != nil {
+				http.Error(w, "error encoding response body", http.StatusInternalServerError)
+				return
+			}
+		},
+	})
+	logger := slog.New(slog.NewJSONHandler(io.Discard, nil))
+	client := NewClient("", server.URL,
+		WithPaths(map[string]string{"testConcurrentLog": "/test/log/concurrent"}),
+		WithLogger(logger),
+		WithLogMWEnabled(true),
+	)
+
+	// ensure no unknown panic occurred
+	defer func() {
+		require.Nil(t, recover())
+	}()
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+	for range 100 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			result := &testLogRes{}
+
+			res, err := client.NewRequest(context.Background()).
+				SetBody(wantReqBody).
+				SetResult(result).
+				Post(client.GetPath("testConcurrentLog"))
+
+			require.NoError(t, err)
+			assert.Equal(t, http.StatusOK, res.StatusCode())
+			assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+			assert.Equal(t, &wantResBody, res.Result())
+		}()
+	}
 }
